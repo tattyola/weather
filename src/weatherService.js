590e5363a2c51e3a15e49e874dc9a12c99dd182b
Tatsiana A <astrouskaya@yahoo.com>
2024-04-03 15:29:34 -0400

    fix dotenv

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,12 +1,8 @@
-
 const getFormattedWeatherData = async (city, units = 'metric') => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=${units}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=${units}`;
     const getIconUrl = (iconId) => `https://openweathermap.org/img/wn/${iconId}.png`
     const data = await fetch(URL)
         .then(res => res.json())
-        // .then(data => data)
-    console.log(data)
-    console.log(process.env.API_KEY)
     const {
         wind: {speed},
         main: {temp, temp_max, temp_min, pressure, humidity, feels_like},
